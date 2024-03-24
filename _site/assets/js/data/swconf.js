f64f4a3c91c788bd5ab4b22bc9842cbecb356ff7
junghyungo <pp90971658@gmail.com>
2024-03-25 02:46:18 +0900

    ㅠㅠㅠㅠ

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1711302197',resources: [
+    cacheName: 'chirpy-1711302345',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
