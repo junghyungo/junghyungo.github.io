a2a37f6fdea5e3ff02f142be58df95836468a114
junghyungo <pp90971658@gmail.com>
2024-03-25 19:24:31 +0900

    jekyll build 1.0

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1711360848',resources: [
+    cacheName: 'chirpy-1711362148',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
